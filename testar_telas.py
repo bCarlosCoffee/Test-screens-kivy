db32d4ade693f95b680ee8eea68e5aa04b1803e9
Carlos Alexandre <51524347+bCarlosCoffee@users.noreply.github.com>
2020-01-14 05:46:10 -0300

    v.1.0.1

---
+++ testar_telas.py
@@ -0,0 +1,481 @@
+screens = {
+	'onex': {
+		'Name': 'HTC One X',
+		'Width': 1280,
+		'Height': 720,
+		'DPI': 312,
+		'Density': 2,
+	},
+
+	'one': {
+		'Name': 'HTC One',
+		'Width': 1920,
+		'Height': 1080,
+		'DPI': 468,
+		'Density': 3,
+	},
+
+	'onesv': {
+		'Name': 'HTC One SV',
+		'Width': 800,
+		'Height': 480,
+		'DPI': 216,
+		'Density': 1.5,
+	},
+
+	's3': {
+		'Name': 'Galaxy SIII',
+		'Width': 1280,
+		'Height': 720,
+		'DPI': 306,
+		'Density': 2,
+	},
+
+
+	'note2': {
+		'Name': 'Galaxy Note II',
+		'Width': 1280,
+		'Height': 720,
+		'DPI': 267,
+		'Density': 2,
+	},
+
+	'droid2': {
+		'Name': 'Motorola Droid 2',
+		'Width': 854,
+		'Height': 480,
+		'DPI': 240,
+		'Density': 1.5,
+	},
+
+	'xoom': {
+		'Name': 'Motorola Xoom',
+		'Width': 1280,
+		'Height': 800,
+		'DPI': 149,
+		'Density': 1,
+	},
+
+	'ipad': {
+		'Name': 'iPad (1 and 2)',
+		'Width': 1024,
+		'Height': 768,
+		'DPI': 132,
+		'Density': 1,
+	},
+
+	'ipad3': {
+		'Name': 'iPad 3',
+		'Width': 2048,
+		'Height': 768,
+		'DPI': 1536,
+		'Density': 2,
+	},
+
+	'iphone4': {
+		'Name':'iPhone 4',
+		'Width':960,
+		'Height':640,
+		'DPI':326,
+		'Density':2,
+	},
+
+	'iphone5': {
+		'Name':'iPhone 5',  
+		'Width':1136,
+		'Height':640,
+		'DPI':326,
+		'Density':2
+	},
+
+	'xperiae':{
+		'Name':'Xperia E',
+		'Width':480,
+		'Height':320,      
+		'DPI':166,
+		'Density':1,
+	},
+
+	'nexus4': {
+		'Name':'Nexus 4',
+		'Width':1280,   
+		'Height':768,     
+		'DPI':320,
+		'Density':2,
+	},
+
+	'nexus7':{
+		'Name':'Nexus 7 (2012 version)',
+		'Width':1280,
+		'Height':800,     
+		'DPI':216,
+		'Density':1.325,
+	},
+	'nexus7.2':{
+		'Name':'Nexus 7 (2013 version)',
+		'Width':1920,
+		'Height':1200,     
+		'DPI':323,
+		'Density':2, 
+	},
+
+	'phone_android_one':{
+		'Name':'Android One',          
+		'Width':854,      
+		'Height':480,      
+		'DPI':218,   
+		'Density':1.5,
+	},     
+	'phone_htc_one_m8':{
+		'Name':'HTC One M8',          
+		'Width':1920,     
+		'Height':1080,     
+		'DPI':432,   
+		'Density':3.0,
+	},     
+	'phone_htc_one_m9':{
+		'Name':'HTC One M9',          
+		'Width':1920,  
+		'Height':1080,
+		'DPI':432,
+		'Density':3.0,
+	},
+	'phone_iphone':{
+		'Name':'iPhone',              
+		'Width':480,      
+		'Height':320,      
+		'DPI':168,
+		'Density':1.0,
+	},
+
+	'phone_iphone_4':{
+		'Name':'iPhone 4',            
+		'Width':960, 
+		'Height':640,      
+		'DPI':320,
+		'Density':2.0,
+	},
+	'phone_iphone_5':{
+		'Name':'iPhone 5',            
+		'Width':1136, 
+		'Height':640,     
+		'DPI':320,
+		'Density':2.0,
+	},
+	'phone_iphone_6':{
+		'Name':'iPhone 6',            
+		'Width':1334, 
+		'Height':750,     
+		'DPI':326,
+		'Density':2.0,
+	},
+	'phone_iphone_6_plus': {
+		'Name':'iPhone 6 Plus',     
+		'Width':1920,
+		'Height':1080,     
+		'DPI':400,
+		'Density':3.0,
+	},
+
+	'phone_lg_g2':{
+		'Name':'LG G2',               
+		'Width':1920,    
+		'Height':1080,     
+		'DPI':432,
+		'Density':3.0,
+	},
+	'phone_lg_g3':{
+		'Name':'LG G3',               
+		'Width':2560,    
+		'Height':1440,     
+		'DPI':533,
+		'Density':3.0,
+	},
+	'phone_moto_g':{
+		'Name':'Moto G',              
+		'Width':1280,   
+		'Height':720,     
+		'DPI':327,
+		'Density':2.0,
+	},
+	'phone_moto_x':{
+		'Name':'Moto X',             
+		'Width':1280,  
+		'Height':720,    
+		'DPI':313,
+		'Density':2.0,
+	},
+
+	'phone_moto_x_2nd_gen':{
+		'Name':'Moto X 2nd Gen',      
+		'Width':1920,
+		'Height':1080,     
+		'DPI':432,
+		'Density':3.0,
+	},
+	'phone_nexus_4':{
+		'Name':'Nexus 4',             
+		'Width':1280,  
+		'Height':768,     
+		'DPI':240,
+		'Density':2.0,
+	},
+	'phone_nexus_5': {
+		'Name':'Nexus 5',            
+		'Width':1920,  
+		'Height':1080,     
+		'DPI':450,
+		'Density':3.0,
+	},
+
+
+	'phone_nexus_5x': {
+		'Name':'Nexus 5X',           
+		'Width':1920, 
+		'Height':1080,     
+		'DPI':432,
+		'Density':2.6,
+	},
+
+	'phone_nexus_6': {
+		'Name':'Nexus 6',            
+		'Width':2560,  
+		'Height':1440,     
+		'DPI':496,
+		'Density':3.5,
+	},
+
+	'phone_nexus_6p': {
+		'Name':'Nexus 6P',           
+		'Width':2560, 
+		'Height':1440,     
+		'DPI':514,
+		'Density':3.5,
+	},
+
+	'phone_oneplus_3t':{
+		'Name':'OnePlus 3t',          
+		'Width':1863,
+		'Height':1080,     
+		'DPI':380,
+		'Density':2.375,
+	},
+
+	'phone_samsung_galaxy_note_4':{
+		'Name':'Samsung Galaxy Note 4',
+		'Width':2560,
+		'Height':1440,     
+		'DPI':514,
+		'Density':3.0,
+	},
+
+	'phone_samsung_galaxy_s5':{
+		'Name':'Samsung Galaxy S5',   
+		'Width':1920,
+		'Height':1080,     
+		'DPI':372,
+		'Density':3.0,
+	},
+
+	'phone_samsung_galaxy_s6':{
+		'Name':'Samsung Galaxy S6',    
+		'Width':2560, 
+		'Height':1440,     
+		'DPI':576,
+		'Density':4.0,
+	},
+
+	'phone_sony_xperia_c4': {
+		'Name':'Sony Xperia C4',     
+		'Width':1920,
+		'Height':1080,     
+		'DPI':400,
+		'Density':2.0,
+	},
+	'phone_sony_xperia_z_ultra':{
+		'Name':'Sony Xperia Z Ultra', 
+		'Width':1920,
+		'Height':1080,     
+		'DPI':348,
+		'Density':2.0,
+	},
+	'phone_sony_xperia_z1_compact':{
+		'Name':'Sony Xperia Z1 Compact',
+		'Width':1280,
+		'Height':720,     
+		'DPI':342,
+		'Density':2.0,
+	},
+		'phone_sony_xperia_z2z3':{
+		'Name':'Sony Xperia Z2/Z3',    
+		'Width':1920,
+		'Height':1080,     
+		'DPI':432,
+		'Density':3.0,
+	},
+		'phone_sony_xperia_z3_compact':{
+		'Name':'Sony Xperia Z3 Compact',
+		'Width':1280,
+		'Height':720,     
+		'DPI':313,
+		'Density':2.0,
+	},
+	'tablet_dell_venue_8':{
+		'Name':'Dell Venue 8',      
+		'Width':2560,
+		'Height':1600,     
+		'DPI':355,
+		'Density':2.0,
+	},
+	'tablet_ipad': {
+		 'Name':'iPad',               
+		 'Width':1024,     
+		 'Height':768,     
+		 'DPI':132,
+		 'Density':1.0,
+	},     
+	'tablet_ipad_mini':{
+		'Name':'iPad Mini',          
+		'Width':1024,
+		'Height':768,     
+		'DPI':163,
+		'Density':1.0,
+	},
+	'tablet_ipad_mini_retina':{
+		'Name':'iPad Mini Retina',   
+		'Width':2048,
+		'Height':1536,     
+		'DPI':326,
+		'Density':2.0,
+	},
+	'tablet_ipad_pro':{
+		'Name':'iPad Pro',           
+		'Width':2732, 
+		'Height':2048,     
+		'DPI':265,
+		'Density':2.0,
+	},     
+	'tablet_ipad_retina':{
+		'Name':'iPad Retina',     
+		'Width':2048,
+		'Height':1536,     
+		'DPI':264,
+		'Density':2.0,
+	},
+	'tablet_nexus_10':{
+		'Name':'Nexus 10',           
+		'Width':2560, 
+		'Height':1600,     
+		'DPI':297,
+		'Density':2.0,
+	},
+	'tablet_nexus_7_12':{
+		'Name':'Nexus 7 12',         
+		'Width':1280,
+		'Height':800,     
+		'DPI':216,
+		'Density':1.3,
+	},
+	'tablet_nexus_7_13':{
+		'Name':'Nexus 7 13',         
+		'Width':1920,
+		'Height':1200,     
+		'DPI':324,
+		'Density':2.0,
+	},
+	'tablet_nexus_9':{
+		'Name':'Nexus 9',            
+		'Width':2048,  
+		'Height':1536,     
+		'DPI':288,
+		'Density':2.0,
+	},     
+	'tablet_samsung_galaxy_tab_10': {
+		'Name':'Samsung Galaxy Tab 10',
+		'Width':1280,
+		'Height':800,     
+		'DPI':148,
+		'Density':1.0,
+	},
+	'tablet_sony_xperia_z3_tablet':{
+		'Name':'Sony Xperia Z3 Tablet',
+		'Width':1920,
+		'Height':1200,     
+		'DPI':282,
+		'Density':2.0,
+	},
+	'tablet_sony_xperia_z4_tablet':{
+		'Name':'Sony Xperia Z4 Tablet',
+		'Width':2560,
+		'Height':1600,     
+		'DPI':297,
+		'Density':2.0,
+	},
+
+	'tablet_huawei_mediapad_m3_lite_10':{
+		'Name':'HUAWEI MediaPad M3 Lite 10',
+		'Width':1920,   
+		'Height':1200,
+		'DPI':320,
+		'Density':2.25,
+	},
+}
+
+
+
+from os import system
+
+def testar(arquivo="main.py", screen="", portrait='portrait', scale=.65):
+
+	system(
+		f'python {arquivo} -m screen:{screen},{portrait},scale={scale}'
+	)
+
+test_all_screen = False
+for screen in screens.keys():
+	print(
+		"""
+		Screen to be used:
+
+		Name: {name}
+		Width: {width}
+		Height: {height}
+		DPI: {dpi}
+		Density: {density}
+
+
+		Commands:
+			[ s ] = Skip to another screen.
+			[ e ] = Exit.
+			[ tall ] = No more question and test all screens.
+
+			Anyone to continue.
+
+		""".format(
+			name=screens[screen]['Name'],
+			width=screens[screen]['Width'],
+			height=screens[screen]['Height'],
+			dpi=screens[screen]['DPI'],
+			density=screens[screen]['Density']
+		)
+	)
+
+
+	if test_all_screen == False:
+		choice = input('#~>  ').strip().lower()
+
+		if choice == 'e':
+			break
+
+		if choice == 'tall':
+			test_all_screen = True
+
+		if choice == 's':
+			continue
+
+
+	try:
+		testar(screen=screen)
+
+	except KeyboardInterrupt:
+		break
